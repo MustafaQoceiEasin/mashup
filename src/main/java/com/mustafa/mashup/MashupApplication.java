14a8fa8720728f7163502f50aa193c60d7868f90
MustafaQoceiEasin <dominusinstructions@gmail.com>
2022-04-07 03:53:37 +0200

    removed unused method

--- src/main/java/com/mustafa/mashup/MashupApplication.java
+++ src/main/java/com/mustafa/mashup/MashupApplication.java
@@ -10,23 +10,10 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableAsync
 public class MashupApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MashupApplication.class, args);
-	}
-
-
-	@Bean
-	public Executor coverArtExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(2);
-		executor.setMaxPoolSize(2);
-		executor.setQueueCapacity(500);
-		executor.setThreadNamePrefix("CoverArtExecutor-");
-		executor.initialize();
-		return executor;
 	}
 
 }
